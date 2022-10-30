288d2b631153727cb3f698e9c42f5f7e815562d5
Дмитрий Еланский <elan.dmitriy@gmail.com>
2022-10-30 20:52:20 +0300

    #7. Модели

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,14 @@
+from tokenize import blank_re
+from xml.etree.ElementInclude import default_loader
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # пока не работает (news.News.photo: (fields.E210) Cannot use ImageField because Pillow is not installed.)
+    # photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_publushed = models.BooleanField(default=True)
+
+
