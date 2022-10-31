37fb74bb747f9bcbe773a0a0d46f390c5429c88b
Дмитрий Еланский <elan.dmitriy@gmail.com>
2022-10-31 16:48:12 +0300

    #11. Шаблоны

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from news.models import News
 
 def index(request):
-    print(request)
-    return HttpResponse('Hellow world')
+    template = "news/index.html"
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template, context)
 
 def test(request):
      return HttpResponse('Тестовая страница')
